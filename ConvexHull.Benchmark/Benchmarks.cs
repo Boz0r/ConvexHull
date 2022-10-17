af2ed39a7ef7560871180a9bd38f10b21ebb6ea2
Boz0r <pighat@gmail.com>
2022-10-17 21:03:23 +0200

    Benchmark for bridge strategy

--- ConvexHull.Benchmark/Benchmarks.cs
+++ ConvexHull.Benchmark/Benchmarks.cs
@@ -9,6 +9,7 @@ public class Benchmarks
 {
     private readonly IConvexHullAlgorithm incrementalAlgorithm = new IncrementalAlgorithm();
     private readonly IConvexHullAlgorithm naiveAlgorithm = new NaiveAlgorithm();
+    private readonly IBridgeStrategy bridgeStrategy = new DefaultBridgeStrategy();
 
     [Benchmark, Arguments(10), Arguments(100), Arguments(1000)]
     public List<Vector3> IncrementalAlgorithm(int count)
@@ -16,11 +17,19 @@ public class Benchmarks
         var input = new Fixture().CreateMany<Vector3>(count).ToList();
         return incrementalAlgorithm.Compute(input);
     }
-
+    
     [Benchmark, Arguments(10), Arguments(100), Arguments(1000)]
     public List<Vector3> NaiveAlgorithm(int count)
     {
         var input = new Fixture().CreateMany<Vector3>(count).ToList();
         return naiveAlgorithm.Compute(input);
+    }
+
+    [Benchmark, Arguments(1), Arguments(10), Arguments(100), Arguments(1000)]
+    public (Vector3 i, Vector3 j) DefaultBridgeStrategy(int count)
+    {
+        var input = new Fixture().CreateMany<Vector3>(count).ToList();
+
+        return bridgeStrategy.Bridge(input, 1);
     }
 }
