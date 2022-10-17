a549299b410d42f9476f1d179ddc8f7c487fdc34
Boz0r <pighat@gmail.com>
2022-10-17 21:00:00 +0200

    Moved AlgorithmTestData to own file

---
+++ ConvexHull.Test/AlgorithmTestData.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Numerics;
+
+namespace ConvexHull.Test;
+
+public class AlgorithmTestData : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[]
+        {
+            new List<Vector3>
+            {
+                new(3, 0, 0),
+                new(1, 1, 0),
+                new(0, 0, 0),
+                new(2, 0, 0),
+                new(1, 0, 0),
+                new(2, 1, 0),
+                new(1, -1, 0),
+                new(2, -1, 0),
+            },
+            new List<Vector3>
+            {
+                new(0, 0, 0),
+                new(1, -1, 0),
+                new(1, 1, 0),
+                new(2, -1, 0),
+                new(2, 1, 0),
+                new(3, 0, 0),
+            },
+        };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => GetEnumerator();
+}
